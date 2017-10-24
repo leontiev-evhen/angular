b7316daf0a1dd0359bd85182cd6222e8f075df59
student <student@GFL-1569.ad.geeksforless.net>
2017-10-24 20:28:41 +0300

    use factory

--- lesson1/app/main.js
+++ lesson1/app/main.js
@@ -1,17 +1,78 @@
-var app = angular.module("app", []);
-app.controller('FirstController', FirstController)
-function FirstController ($scope) {
-	var self = this;
-	this.lesson = 'AngularJS lesson #1'
-	this.data = [];
+angular.module('app', [])
+/*.controller('Controller', ['$scope', function($scope) {
+	$scope.name = 'Yevhen';
+}])
 
-	this.addItem = function (item) {
-		self.data.push(item);
-		$scope.newItem = '';
-	}
-}
+.directive('myDialog', function() {
+	return {
+		restrict: 'E',
+		transclude: true,
+		scope: {
+			newName: '@?newName'
+		},
+		template: 'Hello, my name is {{newName}}'
+  };
+})
 
-app.controller('SecondController', SecondController)
-function SecondController ($scope) {
-	this.months = ['mart', 'april', 'may', 'june', 'july', 'august'];
-}
+.directive('myDialogController', function() {
+	return {
+		restrict: 'E',
+		transclude: true,
+		scope: {
+			newName: '@?newName'
+		},
+		controller: ['$scope', function MyTabsController($scope) {
+			console.log($scope.newName)
+		}],
+		template: 'Hello, my name is {{newName}}'
+  };
+})*/
+
+
+.factory('addProducts', ['$window', function() {
+	products = [];
+	return {
+		add: function(item) {
+			products.push(item);
+		},
+		get: function() {
+			return products;
+		},
+	};
+ }])
+ 
+
+.directive('myInput', function() {
+	
+	return {
+		restrict: 'E',
+		transclude: true,
+		controller: ['$scope', 'addProducts', function MyController($scope, addProducts) {
+				$scope.addItem = function(item) {
+					addProducts.add(item);
+					$scope.item = '';
+				};
+		}],
+		
+		template: '<p><input type="text" ng-model="item"><button ng-click="addItem(item);">Add</button></p>'
+		
+	};
+})
+
+.directive('list', function() {
+	
+	return {
+		restrict: 'E',
+		transclude: true,
+		
+		controller: ['$scope', 'addProducts', function MyController($scope, addProducts) {
+			$scope.prod = addProducts.get();
+		}],
+		template: '<ul><li ng-repeat="item in prod track by $index">{{item}}</li></ul>'
+		
+	};
+})
+
+
+
+
