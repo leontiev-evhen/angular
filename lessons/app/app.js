2561552ba85623a1dec745ab102f75a716d1e82e
Yevhen <leontevevgenii@gmail.com>
2017-10-26 21:50:50 +0300

    add validation name

--- lessons/app/app.js
+++ lessons/app/app.js
@@ -1,15 +1,21 @@
-angular.module('app', []);
-
-app.directive('username', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-		ctrl.$validators.integer = function(modelValue, viewValue) {
-			var usernames = ['Jim', 'John', 'Jill', 'Jackie'];
-			if (usernames.indexOf(modelValue) === -1) {
-				
-			}			
-		}
-		
-  };
+angular.module('app', [])
+.controller('MainCtrl', function($scope) {
+	$scope.error = true;
+})
+.directive('username', function() {
+  	return {
+	    require: 'ngModel',
+	    link: function(scope, elm, attrs, ctrl) {
+	      	var usernames = ['Jim', 'John', 'Jill', 'Jackie'];
+	      	ctrl.$validators.username = function(modelValue, viewValue) {
+	      	
+		    	if (usernames.indexOf(modelValue) != -1 || ctrl.$isEmpty(modelValue)) {
+	        		scope.error = true;
+	         	} else {
+	            	scope.error = false;
+	          	}
+			    
+	     	};
+	    }
+  	};
 });
